39b36669c81a20c8db5d5009c4a00746dd2c4e6c
Raman Gupta <raman@vivosys.com>
2014-08-23 11:07:15 -0400

    Update test code, use one spec file and test mechanism

--- src/test/scala/TestCountingService.scala
+++ src/test/scala/TestCountingService.scala
@@ -1,1 +1,15 @@
-// see the tutorial
+import com.typesafe.config.Config
+import java.util.concurrent.atomic.AtomicInteger
+import javax.inject.Inject
+import sample.CountingService
+
+class TestCountingService @Inject() (config: Config) extends CountingService(config) {
+  private val called = new AtomicInteger(0)
+
+  override def increment(count: Int) = {
+    called.incrementAndGet()
+    super.increment(count)
+  }
+
+  def getNumberOfCalls: Int = called.get()
+}
